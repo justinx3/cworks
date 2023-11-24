364b0c15e11ef369d06579cac025dfda2cb80f9f
Justin x3 <124699631+justinx3@users.noreply.github.com>
2023-11-24 12:17:07 +0530

    Add files via upload

---
+++ labworks/NF.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,m,n,a[20],b[20],p,q;
+clrscr();
+printf("Enter the rows and coloums of the first array:");
+scanf("%d",&m &n);
+printf("Enter the rows and colounms of the second array:");
+scanf("%d",&p &q);
+printf("\nEnter the elements of the first array:");
+for(i=0;i
